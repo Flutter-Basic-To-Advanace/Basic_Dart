4cafdec91c423654d6698df995095af2cd20b8ce
sharada417 <sharada.kanchana2001@gmail.com>
2025-08-08 03:35:08 +0530

    resore file

---
+++ 2.variable.dart
@@ -0,0 +1,14 @@
+void main() {
+  // Basic types
+  int age = 21;
+  double height = 5.9;
+  String name = "Sharada";
+  bool isStudent = true;
+
+  // Printing
+  print("Name: $name");
+  print("Age: $age");
+  print("Height: $height");
+  print("Student: $isStudent");
+  
+}
